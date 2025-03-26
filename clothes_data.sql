f9187cff48d913c9bfc790b72cfa15221d30b0dd
Cherry-pooki <66070327@kmitl.ac.th>
2025-03-26 17:10:19 +0700

    Delete clothes_data.sql

--- clothes_data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO CLOTHES (clothes_id, clothes_type, rate) VALUES (1, 'Shirt', 20.00);
-INSERT INTO CLOTHES (clothes_id, clothes_type, rate) VALUES (2, 'T-Shirt', 20.00);
-INSERT INTO CLOTHES (clothes_id, clothes_type, rate) VALUES (3, 'Pants', 25.00);
-INSERT INTO CLOTHES (clothes_id, clothes_type, rate) VALUES (4, 'Suit', 60.00);
-INSERT INTO CLOTHES (clothes_id, clothes_type, rate) VALUES (5, 'Dress', 50.00);
-INSERT INTO CLOTHES (clothes_id, clothes_type, rate) VALUES (6, 'Skirt', 25.00);
-INSERT INTO CLOTHES (clothes_id, clothes_type, rate) VALUES (7, 'Blouse', 20.00);
-INSERT INTO CLOTHES (clothes_id, clothes_type, rate) VALUES (8, 'Jacket', 40.00);
-INSERT INTO CLOTHES (clothes_id, clothes_type, rate) VALUES (9, 'Sweater', 30.00);
