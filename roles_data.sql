0a6c6a51b3c3d6c4f0bbff96495e7347d614d724
Cherry-pooki <66070327@kmitl.ac.th>
2025-03-26 17:10:09 +0700

    Delete roles_data.sql

--- roles_data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO ROLES (role_id, role_name) VALUES (1, 'Branch manager');
-INSERT INTO ROLES (role_id, role_name) VALUES (2, 'Delivery staff');
-INSERT INTO ROLES (role_id, role_name) VALUES (3, 'Ironing staff');
-INSERT INTO ROLES (role_id, role_name) VALUES (4, 'Washer/Dryer operator');
-INSERT INTO ROLES (role_id, role_name) VALUES (5, 'Customer service staff');
-INSERT INTO ROLES (role_id, role_name) VALUES (6, 'Cashier');
-INSERT INTO ROLES (role_id, role_name) VALUES (7, 'System Administrator');
-INSERT INTO ROLES (role_id, role_name) VALUES (8, 'Investor');
