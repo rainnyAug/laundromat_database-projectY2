8147af372afa4ef6bc921babde754bd11c46176a
Cherry-pooki <66070327@kmitl.ac.th>
2025-03-26 13:02:07 +0700

    Update data_order_services.sql

--- data_order_services.sql
+++ data_order_services.sql
@@ -5,12 +5,12 @@ MAXVALUE 999999999;
 --order_service_id, order_id, service_id
 
 INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 1, 1);  
-INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 1, 2);  
-INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 2, 1); 
-INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 2, 3); 
-INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 3, 4); 
-INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 4, 1); 
-INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 5, 2); 
-INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 5, 3); 
-INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 6, 1);  
-INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 6, 4);
+INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 2, 2);  
+INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 3, 1); 
+INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 4, 3); 
+INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 5, 4); 
+INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 6, 1); 
+INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 7, 2); 
+INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 8, 3); 
+INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 9, 1);  
+INSERT INTO ORDERS_SERVICE VALUES (order_service_id_seq.nextval, 10, 4);
