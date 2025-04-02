ed69ed9c5d531ccf6f52e518541ba64a9af26d0e
Khokwan Tachaongart <khokwan16@gmail.com>
2025-04-02 18:07:11 +0700

    Update middle5.sql

--- commands/middle/middle5.sql
+++ commands/middle/middle5.sql
@@ -1,6 +1,6 @@
 -- Count the number of services requested per customer, sorted by most active
 SELECT c.customer_id, c.first_name, COUNT(os.order_service_id) AS total_services
-From CUSTOMERS c
+FROM CUSTOMERS c
 JOIN orders o ON c.customer_id = o.customer_id
 JOIN orders_service os ON o.order_id = os.order_id
 GROUP BY c.customer_id, c.first_name
