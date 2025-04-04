36e2e3a83b6e1b30c9bfcdc523e33f985188c52c
Cherry-pooki <66070327@kmitl.ac.th>
2025-04-04 06:27:56 -0700

    editing customer dataset by combining with membership table

--- membership_autoUpdate.sql
+++ membership_autoUpdate.sql
@@ -1,3 +1,5 @@
+-- update_membership_tiers.sql
+-- This script updates customer membership tiers based on their number of completed orders.
 MERGE INTO CUSTOMERS c
 USING (
     SELECT o.customer_id,
@@ -15,9 +17,8 @@ WHEN MATCHED THEN UPDATE SET
         ELSE 'None'
     END,
     c.discount_rate = CASE
-        WHEN t.total_completed >= 40 THEN 10.00
-        WHEN t.total_completed >= 25 THEN 7.50
-        WHEN t.total_completed >= 10  THEN 5.00
-        WHEN t.total_completed >= 1  THEN 2.50
+        WHEN t.total_completed >= 40 THEN 5.00
+        WHEN t.total_completed >= 25 THEN 3.00
+        WHEN t.total_completed >= 10  THEN 2.00
         ELSE 0.00
     END;
